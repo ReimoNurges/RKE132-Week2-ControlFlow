8311806e02a8b9eab42a6b0b688c06cfa39db9a9
reimo <reimo.nurges@tktk.ee>
2024-09-24 14:13:53 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// sugu
+// perekonna nimi   
+// 
+//Welcome, Mr/Ms. [perekonnanimi]
+
+Console.WriteLine("Mis on teie sugu?  m/f");
+char gender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Mis on teie perekonnanimi?");
+string pere = Console.ReadLine();
+if (gender == 'm')
+{
+    string pronoun = ("Mr.");
+    Console.WriteLine($"Welcome, {pronoun} {pere}");
+}
+else if (gender == 'f')
+{
+    string pronoun = ("Ms.");
+    Console.WriteLine($"Welcome, {pronoun} {pere}");
+}
+else Console.WriteLine($"Welcome, {pere}");
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Kui vana te olete?");
+string vanus = Console.ReadLine();
+int vanusArv = 0;
+bool KasonArv = Int32.TryParse(vanus, out vanusArv);
+
+//int vanus = Int32.Parse(Console.ReadLine());
+if (KasonArv == true)
+{
+    if (vanusArv >= 13)
+    {
+        Console.WriteLine("Welcome to instagram");
+    }
+    else
+    {
+        Console.WriteLine("You are too young for instagram");
+    }
+}
+else Console.WriteLine("Sisestage õige vanus");
